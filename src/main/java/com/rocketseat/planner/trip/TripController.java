fa5959c62d69935e081740dc1dd66c702eb6cf82
almeida.veronica <almeida.vrc@gmail.com>
2024-07-10 16:31:55 -0300

    Add trip detail by id

--- src/main/java/com/rocketseat/planner/trip/TripController.java
+++ src/main/java/com/rocketseat/planner/trip/TripController.java
@@ -5,10 +5,10 @@ import com.rocketseat.planner.participant.ParticipantService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/trips")
@@ -31,5 +31,12 @@ public class TripController {
 
         return ResponseEntity.ok(new TripCreateResponse((newTrip.getId())));
 
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id){
+        Optional<Trip> trip = this.repository.findById(id);
+
+        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
