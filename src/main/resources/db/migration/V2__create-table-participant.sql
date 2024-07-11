960d1f207178a693aa60ba975defc5b693308c89
almeida.veronica <almeida.vrc@gmail.com>
2024-07-11 12:08:54 -0300

    Create table participant

---
+++ src/main/resources/db/migration/V2__create-table-participant.sql
@@ -0,0 +1,20 @@
+
+--CREATE TABLE participants (
+--    id UUID DEFAULT RANDOW_UUID() PRIMARY KEY,
+--    name VARCHAR(255) NOT NULL,
+--    email VARCHAR(255) NOT NULL,
+--    is_confirmed BOOLEAN NOT NULL,
+--    trip_id UUID,
+--    FOREIGN KEY (trip_id) REFERENCES trips(id)
+--)
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE participants (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    is_confirmed BOOLEAN NOT NULL,
+    trip_id UUID,
+    FOREIGN KEY (trip_id) REFERENCES trips(id)
+);
