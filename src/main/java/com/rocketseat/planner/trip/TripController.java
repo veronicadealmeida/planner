256796e85a710f3b952319c7a14ea244345979df
almeida.veronica <almeida.vrc@gmail.com>
2024-07-11 11:59:55 -0300

    Creates endponts confirme and put to trips

--- src/main/java/com/rocketseat/planner/trip/TripController.java
+++ src/main/java/com/rocketseat/planner/trip/TripController.java
@@ -2,13 +2,16 @@ package com.rocketseat.planner.trip;
 
 
 import com.rocketseat.planner.participant.ParticipantService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.UUID;
+
 
 @RestController
 @RequestMapping("/trips")
@@ -38,5 +41,40 @@ public class TripController {
         Optional<Trip> trip = this.repository.findById(id);
 
         return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Trip> updateTrip(@PathVariable UUID id, @RequestBody TripRequestPayload payload){
+        Optional<Trip> trip = this.repository.findById(id);
+
+        if(trip.isPresent()){
+            Trip rawTrip = trip.get();
+            rawTrip.setEndsAt(LocalDateTime.parse(payload.ends_at(), DateTimeFormatter.ISO_DATE_TIME));
+            rawTrip.setStartsAt(LocalDateTime.parse(payload.starts_at(), DateTimeFormatter.ISO_DATE_TIME));
+
+            rawTrip.setDestination(payload.destination());
+            this.repository.save(rawTrip);
+
+            return  ResponseEntity.ok(rawTrip);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{id}/confirm")
+    public ResponseEntity<Trip> confirmTrip(@PathVariable UUID id) {
+        Optional<Trip> trip = this.repository.findById(id);
+
+        if (trip.isPresent()) {
+            Trip rawTrip = trip.get();
+            rawTrip.setIsConfirmed((true));
+
+            this.repository.save(rawTrip);
+            this.participantService.triggerConfirmationEmailToParticipants((id));
+
+            return ResponseEntity.ok(rawTrip);
+
+        }
+        return ResponseEntity.notFound().build();
     }
 }
